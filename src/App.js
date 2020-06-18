4832c49229916c19d71af956b5a157e4bff69587
tubt <tu.bui@gooddata.com>
2020-06-18 12:34:11 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ class App extends Component {
         super(props);
 
         const whiteLabeledDomain = "zebroids.intgdc.com";
-        this.sdk = sdkFactory({ domain: whiteLabeledDomain }); // this needs to be provided as a prop to the Visualization component in render method
+        this.sdk = sdkFactory({ domain: whiteLabeledDomain });
+        console.log("sdk", this.sdk); // this needs to be provided as a prop to the Visualization component in render method
         this.projectId = "owaemoi0a81c4cq9ae10cxmt24evmf9a"; // this needs to be project on whitelabeled domain
         this.visId = "289"; // this needs to be some chart visualization NOT table!!!
         // TODO once domain where this app is deployed is enabled for CORS on whitelabeled domain remove any PROXY settings
@@ -59,7 +60,7 @@ class App extends Component {
                             <Visualization
                                 projectId={this.projectId}
                                 uri={`/gdc/md/${this.projectId}/obj/${this.visId}`}
-                                // sdk={this.sdk}
+                                sdk={this.sdk}
                                 onExportReady={this.onExportReady}
                             />
                         </div>
