2025eba8eb8a9966b8b2d20a7c15af87886d9f1c
tubt <tu.bui@gooddata.com>
2020-06-18 11:39:04 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,81 +1,78 @@
-import React, { Component } from 'react';
-import '@gooddata/react-components/styles/css/main.css';
-import {
-  Visualization
-} from '@gooddata/react-components';
-import { factory as sdkFactory } from '@gooddata/gooddata-js';
-import './App.css';
+import React, { Component } from "react";
+import "@gooddata/react-components/styles/css/main.css";
+import { Visualization } from "@gooddata/react-components";
+import { factory as sdkFactory } from "@gooddata/gooddata-js";
+import "./App.css";
 
 const DOWNLOADER_ID = "downloader";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    const whiteLabeledDomain = 'zebroids.intgdc.com';
-    this.sdk = sdkFactory({ domain: whiteLabeledDomain }); // this needs to be provided as a prop to the Visualization component in render method
-    this.projectId = 'ltn06hvt07uko2r87itmnoaibgzc0mkn'; // this needs to be project on whitelabeled domain
-    this.visId = '75548'; // this needs to be some chart visualization NOT table!!!
-    // TODO once domain where this app is deployed is enabled for CORS on whitelabeled domain remove any PROXY settings
+        const whiteLabeledDomain = "zebroids.intgdc.com";
+        this.sdk = sdkFactory({ domain: whiteLabeledDomain }); // this needs to be provided as a prop to the Visualization component in render method
+        this.projectId = "owaemoi0a81c4cq9ae10cxmt24evmf9a"; // this needs to be project on whitelabeled domain
+        this.visId = "290"; // this needs to be some chart visualization NOT table!!!
+        // TODO once domain where this app is deployed is enabled for CORS on whitelabeled domain remove any PROXY settings
 
-    this.doExport = this.doExport.bind(this);
-  }
-
-  onExportReady = exportResult => {
-    this.exportResult = exportResult;
-  };
-
-  downloadFile = uri => {
-    let anchor = document.getElementById(DOWNLOADER_ID);
-    if (!anchor) {
-        anchor = document.createElement("a");
-        anchor.id = DOWNLOADER_ID;
-        document.body.appendChild(anchor);
+        this.doExport = this.doExport.bind(this);
     }
-    anchor.href = uri;
-    anchor.download = uri;
-    anchor.click();
-  };
 
-  exportToCSV = () => {
-      this.doExport({format: "csv" });
-  };
+    onExportReady = exportResult => {
+        this.exportResult = exportResult;
+    };
 
-  async doExport(exportConfig) {
-    try {
-        const result = await this.exportResult(exportConfig);
-        this.setState({ errorMessage: null });
-        this.downloadFile(result.uri);
-    } catch (error) {
-        // error handling
-        console.error(error);
+    downloadFile = uri => {
+        let anchor = document.getElementById(DOWNLOADER_ID);
+        if (!anchor) {
+            anchor = document.createElement("a");
+            anchor.id = DOWNLOADER_ID;
+            document.body.appendChild(anchor);
+        }
+        anchor.href = uri;
+        anchor.download = uri;
+        anchor.click();
+    };
+
+    exportToCSV = () => {
+        this.doExport({ format: "csv" });
+    };
+
+    async doExport(exportConfig) {
+        try {
+            const result = await this.exportResult(exportConfig);
+            this.setState({ errorMessage: null });
+            this.downloadFile(result.uri);
+        } catch (error) {
+            // error handling
+            console.error(error);
+        }
     }
-  }
 
-  render() {
-      return (
-        <div style={{ height: 367 }}>
-          <div className="App">
-            <header className="App-header">
-              <div style={{width: 600, height: 800}}>
-                <Visualization
-                  projectId={this.projectId}
-                  uri={`/gdc/md/${this.projectId}/obj/${this.visId}`}
-                  sdk={this.sdk}
-                  onExportReady={this.onExportReady}
-                />
-              </div>
-            </header>
-          </div>
-            <div style={{ marginTop: 15 }}>
-                <button className="gd-button gd-button-secondary" onClick={this.exportToCSV}>
-                    Export CSV
-                </button>
-
+    render() {
+        return (
+            <div style={{ height: 367 }}>
+                <div className="App">
+                    <header className="App-header">
+                        <div style={{ width: 600, height: 800 }}>
+                            <Visualization
+                                projectId={this.projectId}
+                                uri={`/gdc/md/${this.projectId}/obj/${this.visId}`}
+                                sdk={this.sdk}
+                                onExportReady={this.onExportReady}
+                            />
+                        </div>
+                    </header>
+                </div>
+                <div style={{ marginTop: 15 }}>
+                    <button className="gd-button gd-button-secondary" onClick={this.exportToCSV}>
+                        Test Export CSV
+                    </button>
+                </div>
             </div>
-        </div>
-    );
-  }
+        );
+    }
 }
 
 export default App;
